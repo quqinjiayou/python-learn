6b5bd48687cbd07678849fd686382f04e3eaa896
Qu <quqinjiayou@163.com>
2018-03-06 15:09:12 +0800

    learn from the right ans

--- two_sum.py
+++ two_sum.py
@@ -1,21 +1,19 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        size = len(nums)
-        for i in range(0, size):
-            for j in range(i+1, size):
-                if int(nums[i]) + int(nums[j]) == target :
-                    res = [i, j]
-                    return (i, j)
+        if len(nums) <= 1:
+            return False
+        buff_dict = {}
+        for i in range(len(nums)):
+            if nums[i] in buff_dict:
+                return [buff_dict[nums[i]], i]
+            else:
+                buff_dict[target - nums[i]] = i
 
 s = Solution()
 x = str(raw_input("Please input the nums(seg by ,):"))
 target = int(raw_input("Please input the target: "))
 nums = x.split(",")
+nums =  [int(x) for x in nums]
 
 res = s.twoSum(nums, target)
 print(res)
